776de060845e5141b710ae19bf10bd2a3ebf8b10
Vatsal Joshi <vatz88@gmail.com>
2016-09-10 23:58:51 +0530

    Required files added.

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// Add external css file
+
+
+// Change #myName <h3> tag to <h1>
+
+
+// Change src of #myPic
+
+
+// Add your name
+
+
+// Name center align
+
+
+// Image center
+
+
+// Adjust width of profile pic
+
+
+// Change profile pic shape
+
+
+// Change font size of <p>
+
+
+// Load your details from a external file
+
+
+// Change color of <hr>
+
+
+// See website code on button click
+
+
+// See website text on button click
+
+
+// Toggle #websiteCode
+
+
+// remove class w3-ripple from button
+
+
+// Game action
+
+
+// Reset button
+
