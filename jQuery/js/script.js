dcdb1b92e83b530b7aad25b8bbf93266502136a7
Vatsal Joshi <vatz88@gmail.com>
2016-09-21 10:12:08 +0530

    jQuery resources updated

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -1,35 +1,35 @@
 // Add external css file
-
+// <link href='css/style.css' rel='stylesheet'>
 
 // Change #myName <h3> tag to <h1>
 
 
 // Change src of #myPic
-
+// src="image.jpg"
 
 // Add your name
 
 
 // Name center align
-
+// style="text-align:center"
 
 // Image center
-
+// add class imgCenter
 
 // Adjust width of profile pic
 
 
 // Change profile pic shape
-
+// add class w3-circle
 
 // Change font size of <p>
-
+// style="font-size:20px"
 
 // Load your details from a external file
-
+// details is resources/about_me.text
 
 // Change color of <hr>
-
+// style="border-top:1px solid black"
 
 // See website code on button click
 
@@ -44,7 +44,7 @@
 
 
 // Game action
-
+// hint: use 'click' and 'dblclick' on class 'w3-col'
 
 // Reset button
 
