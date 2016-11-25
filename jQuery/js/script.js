f6518deadb6aec370f3fa877310695a793d7c2d8
Vatsal Joshi <vatz88@gmail.com>
2016-11-25 14:03:50 +0530

    answers added

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -1,50 +1,66 @@
 // Add external css file
-// <link href='css/style.css' rel='stylesheet'>
+$('head').append("<link href='css/style.css' rel='stylesheet'>");
 
 // Change #myName <h3> tag to <h1>
-
+$('#myName').replaceWith("<h1 id='myName'>" + $('#myName').html() + "</h1>");
 
 // Change src of #myPic
-// src="image.jpg"
+$('#myPic').attr('src', 'resources/vatsal.jpg');
 
 // Add your name
-
+$('#myName').text('Vatsal Joshi');
 
 // Name center align
-// style="text-align:center"
+$('#myName').css('text-align', 'center');
 
 // Image center
-// add class imgCenter
+$('#myPic').addClass('imgCenter');
 
 // Adjust width of profile pic
-
+$('#myPic').width('100px');
 
 // Change profile pic shape
-// add class w3-circle
+$('#myPic').addClass('w3-circle');
 
 // Change font size of <p>
-// style="font-size:20px"
+$('p').css('font-size', '20px');
 
 // Load your details from a external file
-// details is resources/about_me.text
+$('#aboutMe').load('resources/about_me.txt');
 
 // Change color of <hr>
-// style="border-top:1px solid black"
+$('hr').css('border-top', '1px solid black');
 
 // See website code on button click
-
-
+$('#showCode').click(function () {
+    $('#CodeTextHere').text('');
+    $('#CodeTextHere').text($('html').html());
+    $('#CodeTextHere').show();
+});
 // See website text on button click
-
+$('#showText').click(function () {
+    $('#CodeTextHere').text('');
+    $('#CodeTextHere').text($('html').text());
+    $('#CodeTextHere').show();
+});
 
 // Toggle #websiteCode
-
+$('#toggleText').on('click', function () {
+    $('#CodeTextHere').toggle();
+});
 
 // remove class w3-ripple from button
-
+$('button').removeClass('w3-ripple');
 
 // Game action
-// hint: use 'click' and 'dblclick' on class 'w3-col'
+$('.w3-col').on('click', function () {
+    $(this).text('O');
+});
+$('.w3-col').on('dblclick', function () {
+    $(this).text('X');
+});
 
 // Reset button
-
+$('#resetGame').click(function () {
+    $('.w3-col').text('');
+});
